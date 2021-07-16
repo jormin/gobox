4513f2c5ee7014784f446feac409568f0f827771
Jormin <jorminxee@gmail.com>
2021-07-17 01:25:40 +0800

    id

--- id/id.go
+++ id/id.go
@@ -1,17 +1,15 @@
 package id
 
 import (
-	"strconv"
-
 	"github.com/go-resty/resty/v2"
 )
 
 // 生成ID
-func NewID() (int64, error) {
+func NewID() string {
 	c := resty.New()
 	res, err := c.R().Get("http://id.wcxst.com")
 	if err != nil {
-		return 0, err
+		return ""
 	}
-	return strconv.ParseInt(string(res.Body()), 10, 64)
+	return string(res.Body())
 }
